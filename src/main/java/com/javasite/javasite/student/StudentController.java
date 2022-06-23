8eb58359fc46d961312d430e816fe3c525e12654
predybalov <gelios87@inbox.ru>
2022-06-23 20:09:48 +0300

    dev

--- src/main/java/com/javasite/javasite/student/StudentController.java
+++ src/main/java/com/javasite/javasite/student/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-import java.util.List; 
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
