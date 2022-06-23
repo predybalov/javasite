8b20b0934773e1eb36e0b4792909b5144aed3901
predybalov <gelios87@inbox.ru>
2022-06-23 20:16:26 +0300

    dev

--- src/main/java/com/javasite/javasite/student/StudentService.java
+++ src/main/java/com/javasite/javasite/student/StudentService.java
@@ -1,13 +1,12 @@
 package com.javasite.javasite.student;
 
-
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Component
+@Service
 public class StudentService {
   
   public List<Student> getStudents() {
