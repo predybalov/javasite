22ba8b6708f26eba97a55dcd77f0e704bcd0acd5
predybalov <gelios87@inbox.ru>
2022-06-24 21:04:49 +0300

    dev

---
+++ src/main/java/com/javasite/javasite/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.javasite.javasite.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.jamal@mail.ru",
+                    LocalDate.of(2000, JANUARY, 15),
+                    21
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@mail.ru",
+                    LocalDate.of(1990, JANUARY, 20),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
