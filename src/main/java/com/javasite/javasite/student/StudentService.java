f31794f7139d4863c2020039938fb4c7290ca4de
predybalov <gelios87@inbox.ru>
2022-06-24 20:53:59 +0300

    dev

--- src/main/java/com/javasite/javasite/student/StudentService.java
+++ src/main/java/com/javasite/javasite/student/StudentService.java
@@ -1,23 +1,21 @@
 package com.javasite.javasite.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
-  
+
+  private final StudentRepository studentRepository;
+
+  @Autowired
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
   public List<Student> getStudents() {
-    return List.of(
-      new Student(
-        1L,
-        "Mariam",
-        "mariam.jamal@mail.ru",
-        LocalDate.of(2000, Month.JANUARY, 15),
-        21
-      )
-    );
+    return studentRepository.findAll();
   }
 }
