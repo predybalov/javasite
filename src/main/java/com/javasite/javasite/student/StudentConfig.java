1d4e75c59737f1f13c25f1afdb8751d37ddc4f70
predybalov <gelios87@inbox.ru>
2022-06-24 21:17:02 +0300

    dev

--- src/main/java/com/javasite/javasite/student/StudentConfig.java
+++ src/main/java/com/javasite/javasite/student/StudentConfig.java
@@ -19,15 +19,13 @@ public class StudentConfig {
             Student mariam = new Student(
                     "Mariam",
                     "mariam.jamal@mail.ru",
-                    LocalDate.of(2000, JANUARY, 15),
-                    21
+                    LocalDate.of(2000, JANUARY, 15)
             );
 
             Student alex = new Student(
                     "Alex",
                     "alex@mail.ru",
-                    LocalDate.of(1990, JANUARY, 20),
-                    21
+                    LocalDate.of(1990, JANUARY, 20)
             );
 
             repository.saveAll(
