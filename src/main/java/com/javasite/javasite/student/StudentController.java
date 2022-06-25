abc04d7518dd71d637810b57a5313c8e8b28d3c8
predybalov <gelios87@inbox.ru>
2022-06-25 12:00:03 +0300

    dev

--- src/main/java/com/javasite/javasite/student/StudentController.java
+++ src/main/java/com/javasite/javasite/student/StudentController.java
@@ -27,4 +27,10 @@ public class StudentController {
     studentService.addNewStudent(student);
 
   }
+
+  @DeleteMapping(path = "{studentId}")
+  public void deleteStudent(
+          @PathVariable("studentId") Long studentId) {
+    studentService.deleteStudent(studentId);
+  }
 }
